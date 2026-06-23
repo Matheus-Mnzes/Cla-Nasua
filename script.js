7a96fee6c49b2a37e67018d8aa20ec57cd9e3979
Matheus de Menezes <matheus.de.mnzes@gmail.com>
2026-06-22 22:18:10 -0300

    Imagem, correções e começo da função

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 var nome = prompt("Qual o seu nome? ")
-document.querySelector('strong#nome').innerHTML = `${nome}`
+document.querySelector('strong#nome').innerHTML = `Seja bem vindo ao clã nasua, ${nome}`
+
+
+function abrirAba(id){
+    document.querySelectorAll(".aba").forEach(aba => {aba.classList.remove("ativa")});
+
+    document.getElementById(id).classList.add("ativa")
+}
