6aeb6162b044cdff7a45d9363e3be80298174ab8
Matheus de Menezes <matheus.de.mnzes@gmail.com>
2026-06-30 23:31:00 -0300

    Ajustes
    Ajustes nas funções e ajustes na aba membros

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function abrirAba(id){
+function abrirAba(id,id2){
     document.querySelectorAll(".aba").forEach(aba => {aba.classList.remove("ativa")});
 
     document.getElementById(id).classList.add("ativa")
+    
+    document.getElementById("sidebar").classList.remove("aberta")
 }
 
 
 function abrirSidebar(){
     document.getElementById("sidebar").classList.toggle("aberta")
-}
-
-function fecharSidebar(){
-    document.getElementById("sidebar").classList.remove("aberta")
 }
 
 
